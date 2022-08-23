5adff3a5a52ad6fa51fe2774c2faf8dbc7e36897
João Pedro <69048604+akajhon@users.noreply.github.com>
2022-08-23 19:33:15 -0300

    Added v1.0

---
+++ post_install.sh
@@ -0,0 +1,310 @@
+#!/usr/bin/env bash
+#
+# pos-os-postinstall.sh - Instalar e configura programas no Pop!_OS (20.04 LTS ou superior)
+#
+# ------------------------------------------------------------------------ #
+#
+# COMO USAR?
+#   $ ./pos-os-postinstall.sh
+#
+# ----------------------------- VARIÁVEIS ----------------------------- #
+
+##URLS
+
+URL_GOOGLE_CHROME="https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
+URL_DRACULA_THEME="https://github.com/dracula/gtk/archive/master.zip"
+URL_DRACULA_ICONTHEME="https://github.com/dracula/gtk/files/5214870/Dracula.zip"
+URL_TEAMS="https://go.microsoft.com/fwlink/p/?LinkID=2112886&clcid=0x409&culture=en-us&country=US"
+URL_CADMUS="https://github.com/josh-richardson/cadmus/releases/download/0.0.3/cadmus.deb"
+
+##DIRETÓRIOS E ARQUIVOS
+
+DIRETORIO_DOWNLOADS="$HOME/Downloads/programas"
+FILE="/home/$USER/.config/gtk-3.0/bookmarks"
+mkdir "$DIRETORIO_DOWNLOADS"
+
+
+#CORES
+
+VERMELHO='\e[1;91m'
+VERDE='\e[1;92m'
+SEM_COR='\e[0m'
+
+
+#FUNÇÕES
+
+# Atualizando repositório e fazendo atualização do sistema
+
+apt_update(){
+  sudo apt update && sudo apt dist-upgrade -y
+}
+
+# -------------------------------------------------------------------------------- #
+# -------------------------------TESTES E REQUISITOS----------------------------------------- #
+
+# Internet conectando?
+testes_internet(){
+if ! ping -c 1 8.8.8.8 -q &> /dev/null; then
+  echo -e "${VERMELHO}[ERROR] - Seu computador não tem conexão com a Internet. Verifique a rede.${SEM_COR}"
+  exit 1
+else
+  echo -e "${VERDE}[INFO] - Conexão com a Internet funcionando normalmente.${SEM_COR}"
+fi
+}
+
+# ------------------------------------------------------------------------------ #
+
+
+## Removendo travas eventuais do apt ##
+travas_apt(){
+  sudo rm /var/lib/dpkg/lock-frontend
+  sudo rm /var/cache/apt/archives/lock
+}
+
+## Adicionando/Confirmando arquitetura de 32 bits ##
+add_archi386(){
+sudo dpkg --add-architecture i386
+}
+## Atualizando o repositório ##
+just_apt_update(){
+sudo add-apt-repository ppa:transmissionbt/ppa
+sudo add-apt-repository ppa:agornostal/ulauncher
+sudo apt update -y
+}
+
+
+##DEB SOFTWARES TO INSTALL
+
+PROGRAMAS_PARA_INSTALAR=(
+  snapd
+  ratbagd
+  gparted
+  timeshift
+  virtualbox
+  gufw
+  synaptic
+  vlc
+  code
+  gnome-sushi 
+  folder-color
+  git
+  ubuntu-restricted-extras
+  zsh
+  vim
+  neovim
+  tlp 
+  tlp-rdw
+  htop
+  neofetch
+  gnome-tweaks
+  ulauncher
+  xclip
+  transmission
+  transmission-qt
+  terminator
+)
+
+# ---------------------------------------------------------------------- #
+
+## Download e instalaçao de programas externos ##
+
+install_debs(){
+
+echo -e "${VERDE}[INFO] - Baixando pacotes .deb${SEM_COR}"
+
+mkdir "$DIRETORIO_DOWNLOADS"
+wget -c "$URL_GOOGLE_CHROME"       -P "$DIRETORIO_DOWNLOADS"
+wget -c "$URL_TEAMS"       -P "$DIRETORIO_DOWNLOADS"
+wget -c "$URL_CADMUS"       -P "$DIRETORIO_DOWNLOADS"
+
+
+## Instalando pacotes .deb baixados na sessão anterior ##
+echo -e "${VERDE}[INFO] - Instalando pacotes .deb baixados${SEM_COR}"
+sudo dpkg -i $DIRETORIO_DOWNLOADS/*.deb
+
+# Instalar programas no apt
+echo -e "${VERDE}[INFO] - Instalando pacotes apt do repositório${SEM_COR}"
+
+for nome_do_programa in ${PROGRAMAS_PARA_INSTALAR[@]}; do
+  if ! dpkg -l | grep -q $nome_do_programa; then # Só instala se já não estiver instalado
+    sudo apt install "$nome_do_programa" -y
+  else
+    echo "[INSTALADO] - $nome_do_programa"
+  fi
+done
+
+}
+## Instalando pacotes Flatpak ##
+install_flatpaks(){
+
+  echo -e "${VERDE}[INFO] - Instalando pacotes flatpak${SEM_COR}"
+
+flatpak install flathub com.discordapp.Discord -y
+flatpak install flathub com.uploadedlobster.peek -y
+flatpak install flathub org.kde.okular -y
+flatpak install flathub org.videolan.VLC -y
+flatpak install flathub md.obsidian.Obsidian -y
+flatpak install flathub com.spotify.Client -y
+flatpak install flathub com.bitwarden.desktop -y
+flatpak install flathub org.onlyoffice.desktopeditors -y
+flatpak install flathub org.flameshot.Flameshot -y
+flatpak install flathub org.gnome.Connections -y
+flatpak install flathub cc.arduino.IDE2 -y
+}
+
+## Instalando pacotes Snap ##
+
+install_snaps(){
+
+echo -e "${VERDE}[INFO] - Instalando pacotes snap${SEM_COR}"
+
+sudo snap install pycharm-community --classic 
+
+}
+
+conf_theme(){
+	#Download Dracula Themes
+	wget -c "$URL_DRACULA_THEME"       -P "$DIRETORIO_DOWNLOADS"
+	wget -c "$URL_DRACULA_ICONTHEME"       -P "$DIRETORIO_DOWNLOADS"
+	
+	#Moving to /usr/share/themes and /usr/share/icons
+	unzip gtk-master.zip -d /usr/share/themes
+	unzip Dracula.zip -d /usr/share/icons 
+	
+	#Gnome Theme
+	gsettings set org.gnome.desktop.interface gtk-theme "Dracula"
+	gsettings set org.gnome.desktop.wm.preferences theme "Dracula"
+	#Icons
+	gsettings set org.gnome.desktop.interface icon-theme "Dracula"
+}
+
+conf_terminal(){
+	#Download Dracula gnome-Terminal
+	git clone https://github.com/dracula/gnome-terminal "$DIRETORIO_DOWNLOADS"
+	"$HOME/Downloads/programas/gnome-terminal/./install.sh"
+	
+	#Installing OhMyZSH
+	sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+	
+	#INstalling ZINIT
+	sh -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
+	zinit self-update
+	
+	#Installing Spaceship theme
+	git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+	ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+	
+	#Configs
+	mv .zhrc "/home/$USERNAME/"
+	export alias pbcopy='xclip -selection clipboard'
+	export alias pbpaste='xclip -selection clipboard -o'
+	source ~/.zshrc
+	echo "All the ZSH configurations are Done. Please Reboot."	
+}
+
+conf_terminator(){
+echo 'adding dracula theme' 
+cat <<EOF >  ~/.config/terminator/config
+[global_config]
+  title_transmit_bg_color = "#ad7fa8"
+[keybindings]
+  close_term = <Primary>w
+  close_window = <Primary>q
+  new_tab = <Primary>t
+  new_window = <Primary>i
+  paste = <Primary>v
+  split_horiz = <Primary>e
+  split_vert = <Primary>d
+  switch_to_tab_1 = <Primary>1
+  switch_to_tab_10 = <Primary>0
+  switch_to_tab_2 = <Primary>2
+  switch_to_tab_3 = <Primary>3
+  switch_to_tab_4 = <Primary>4
+  switch_to_tab_5 = <Primary>5
+  switch_to_tab_6 = <Primary>6
+[layouts]
+  [[default]]
+    [[[child1]]]
+      parent = window0
+      type = Terminal
+    [[[window0]]]
+      parent = ""
+      type = Window
+[plugins]
+[profiles]
+  [[default]]
+    cursor_color = "#aaaaaa"
+EOF
+
+
+cat <<EOF >>  ~/.config/terminator/config
+[[Dracula]]
+    background_color = "#1e1f29"
+    background_darkness = 0.88
+    background_type = transparent
+    copy_on_selection = True
+    cursor_color = "#bbbbbb"
+    foreground_color = "#f8f8f2"
+    palette = "#000000:#ff5555:#50fa7b:#f1fa8c:#bd93f9:#ff79c6:#8be9fd:#bbbbbb:#555555:#ff5555:#50fa7b:#f1fa8c:#bd93f9:#ff79c6:#8be9fd:#ffffff"
+    scrollback_infinite = True
+EOF
+}
+
+
+# -------------------------------------------------------------------------- #
+# ----------------------------- PÓS-INSTALAÇÃO ----------------------------- #
+
+
+## Finalização, atualização e limpeza##
+
+system_clean(){
+
+apt_update -y
+flatpak update -y
+sudo apt autoclean -y
+sudo apt autoremove -y
+nautilus -q
+}
+
+
+# -------------------------------------------------------------------------- #
+# ----------------------------- CONFIGS EXTRAS ----------------------------- #
+
+#Mapeia outro HD para ser montado automaticamente
+extra_config(){
+
+#enabling workspaces for both screens
+gsettings set org.gnome.mutter workspaces-only-on-primary false
+
+#Ulauncher Dracula Theme
+git clone https://github.com/dracula/ulauncher.git ~/.config/ulauncher/user-themes/dracula-ulauncher
+
+#Ngrok
+sh -c "$(curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo deb https://ngrok-agent.s3.amazonaws.com buster main | sudo tee /etc/apt/sources.list.d/ngrok.list && sudo apt update && sudo apt install ngrok)"
+
+
+}
+
+# -------------------------------------------------------------------------------- #
+# -------------------------------EXECUÇÃO----------------------------------------- #
+
+travas_apt
+testes_internet
+travas_apt
+apt_update
+travas_apt
+add_archi386
+just_apt_update
+install_debs
+install_flatpaks
+install_snaps
+extra_config
+conf_theme
+conf_terminator
+conf_terminal
+apt_update
+system_clean
+
+## finalização
+
+  echo -e "${VERDE}[INFO] - Script finalizado, instalação concluída! :)${SEM_COR}"
